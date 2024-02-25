0edad2951141c3fde57c70644b700fe1ed559d41
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2024-02-25 16:43:44 +0200

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,50 @@
-import React, { Component } from 'react';
-import Header from './components/header/Header.jsx';
-import Calendar from './components/calendar/Calendar.jsx';
+import React, { useState } from "react";
+import Header from "./components/header/Header.jsx";
+import Calendar from "./components/calendar/Calendar.jsx";
 
-import { getWeekStartDate, generateWeekRange } from '../src/utils/dateUtils.js';
+import { getWeekStartDate, generateWeekRange } from "../src/utils/dateUtils.js";
 
-import './common.scss';
+import "./common.scss";
 
-class App extends Component {
-  state = {
-    weekStartDate: new Date(),
-  };
+function App() {
+  const [weekStartDate, setWeekStartDate] = useState(getWeekStartDate(new Date()));
+  const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
 
-  render() {
-    const { weekStartDate } = this.state;
-    const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
-
-    return (
-      <>
-        <Header />
-        <Calendar weekDates={weekDates} />
-      </>
+  function nextWeek() {
+    const nextWeekStartDay = new Date(weekStartDate);
+    nextWeekStartDay.setDate(
+      weekStartDate.getDate() + ((7 - weekStartDate.getDay() + 1) % 7 || 7)
     );
+    setWeekStartDate(nextWeekStartDay);
   }
+
+  function lastWeek() {
+    const lastWeekStartDay = new Date(weekStartDate);
+    lastWeekStartDay.setDate(
+      weekStartDate.getDate() - ((7 - weekStartDate.getDay() + 1) % 7 || 7)
+    );
+    setWeekStartDate(lastWeekStartDay);
+  }
+
+  function thisWeek() {
+    setWeekStartDate(getWeekStartDate(new Date()));
+  }
+
+  const weekEndDate = new Date(weekStartDate);
+  weekEndDate.setDate(weekStartDate.getDate() + 6);
+
+  return (
+    <>
+      <Header
+        nextWeek={nextWeek}
+        lastWeek={lastWeek}
+        thisWeek={thisWeek}
+        weekStartDate={weekStartDate}
+        weekEndDate={weekEndDate}
+      />
+      <Calendar weekDates={weekDates} />
+    </>
+  );
 }
 
 export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React from "react";
+import { months } from "../../utils/dateUtils";
 
-import './header.scss';
+import "./header.scss";
 
-const Header = () => {
+const Header = ({
+  nextWeek,
+  lastWeek,
+  thisWeek,
+  weekStartDate,
+  weekEndDate,
+}) => {
   return (
     <header className="header">
       <button className="button create-event-btn">
         <i className="fas fa-plus create-event-btn__icon"></i>Create
       </button>
       <div className="navigation">
-        <button className="navigation__today-btn button">Today</button>
-        <button className="icon-button navigation__nav-icon">
+        <button className="navigation__today-btn button" onClick={thisWeek}>
+          Today
+        </button>
+        <button className="icon-button navigation__nav-icon" onClick={lastWeek}>
           <i className="fas fa-chevron-left"></i>
         </button>
-        <button className="icon-button navigation__nav-icon">
+        <button className="icon-button navigation__nav-icon" onClick={nextWeek}>
           <i className="fas fa-chevron-right"></i>
         </button>
-        <span className="navigation__displayed-month"></span>
+        <span className="navigation__displayed-month">
+          {months[weekStartDate.getMonth()].slice(0, 3) ===
+          months[weekEndDate.getMonth()].slice(0, 3)
+            ? months[weekStartDate.getMonth()].slice(0, 3)
+            : months[weekStartDate.getMonth()].slice(0, 3) +
+              " - " +
+              months[weekEndDate.getMonth()].slice(0, 3)}
+        </span>
       </div>
     </header>
   );
