3126f88f3d46f7f745322715f5e1daf8a60b86bc
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2024-05-31 16:38:59 +0300

    add animation for events

--- src/components/event/Event.jsx
+++ src/components/event/Event.jsx
@@ -6,12 +6,12 @@ import { deleteEvent } from '../../gateway/index.js';
 import './event.scss';
 
 const Event = ({ height, marginTop, title, time, id, description, events, setEvent }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
   const eventStyle = {
     height,
     marginTop,
   };
-
-  const [eventClicked, setEventClicked] = useState(false);
 
   const removeEvent = eventId => {
     const thisEvent = events.find(event => event.id === eventId);
@@ -21,29 +21,31 @@ const Event = ({ height, marginTop, title, time, id, description, events, setEve
       alert('You can`t delete an event less than 15 minutes before it starts and after it ends');
       return;
     }
-    deleteEvent(eventId)
-      .then(() => {
-        setEvent(prevState => prevState.filter(element => element.id !== eventId));
-      })
-      .catch(() => {
-        alert('Internal Server Error. Can`t delete event');
-      });
+
+    setIsVisible(false);
+
+    setTimeout(() => {
+      deleteEvent(eventId)
+        .then(() => {
+          setEvent(prevState => prevState.filter(element => element.id !== eventId));
+        })
+        .catch(() => {
+          alert('Internal Server Error. Can`t delete event');
+        });
+    }, 300);
   };
 
   return (
-    <div style={eventStyle} className="event" onClick={() => setEventClicked(!eventClicked)}>
-  <div className="event-information">
-    <div className="event__title">{title}</div>
-    <div className="event__time">{time}</div>
-    <div className="event__description">{description}</div>
-  </div>
-  {eventClicked && (
-    <button className="delete-event-btn" onClick={() => removeEvent(id)}>
-      <FontAwesomeIcon icon={faTrashCan} /> {'Remove event'}
-    </button>
-  )}
-</div>
-
+    <div style={eventStyle} className={`event ${isVisible ? '' : 'fade-out'}`}>
+      <div className="event-information">
+        <div className="event__title">{title}</div>
+        <div className="event__time">{time}</div>
+        <div className="event__description">{description}</div>
+      </div>
+      <button className="delete-event-btn" onClick={() => removeEvent(id)}>
+        <FontAwesomeIcon icon={faTrashCan} /> {'Remove event'}
+      </button>
+    </div>
   );
 };
 
